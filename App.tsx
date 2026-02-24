fd88f604bd216d3fa12c9cc17512b556288e02d3
Thurunu Hashitha <106402728+Thurunuhashitha@users.noreply.github.com>
2026-02-24 20:29:28 +0530

    create login screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { View } from "react-native";
 // import Register from "./src/screens/Register/Register";
-// import Login from './src/screens/Login/Login';
-import Expenses from './src/screens/Expenses/Expenses'
+import Login from './src/screens/Login/Login';
+// import Expenses from './src/screens/Expenses/Expenses'
 
 function App() { 
 
   return ( 
     <View>
-      {/* <Register/> */}
-      {/* <Login /> */}
-      <Expenses />
+      {/* <Register /> */}
+      <Login />
+      {/* <Expenses /> */}
     </View>
   );
 }
