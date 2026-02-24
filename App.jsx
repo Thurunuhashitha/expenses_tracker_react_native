1a76f3a481172115a742a82d1e659871fb6af4ca
Thurunu Hashitha <106402728+Thurunuhashitha@users.noreply.github.com>
2026-02-24 19:58:33 +0530

    create register screen

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import Expenses from './src/screens/Expenses/Expenses';
+import Login from './src/screens/Login/Login'
+import Register from './src/screens/Register/Register'
+
+function App() {
+  return (
+    <View>
+      <Register />
+      {/* <Login /> */}
+      {/* <Expenses /> */}
+    </View>
+  );
+}
+
+export default App;
+ 
